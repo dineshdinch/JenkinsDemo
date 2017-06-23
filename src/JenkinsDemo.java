e057b1984663f4476799c1c9839a43a47031342d
devi <devi.sambandhan@treselle.com>
2017-06-23 11:28:03 +0530

    auto commit

--- src/JenkinsDemo.java
+++ src/JenkinsDemo.java
@@ -2,8 +2,8 @@ public class JenkinsDemo {
     public static void main(String args[]){
         System.out.println("Hello!! This is my first program in Jenkins");
 		
-		System.out.println("Compiling the source code automatically...."
+		System.out.println("Compiling the source code automatically....");
 		System.out.println("This is done using Git Hooks(post-commit)");
-		System.out.println("End!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("End!!!!!!!!!!!!!!!!!!!!!!!!");
     }
 }
