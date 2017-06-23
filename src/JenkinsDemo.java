377e06848df39e1733a6a51caacc016d6397508b
devi <devi.sambandhan@treselle.com>
2017-06-23 09:39:51 +0530

    checking compile with post-commit

--- src/JenkinsDemo.java
+++ src/JenkinsDemo.java
@@ -1,5 +1,8 @@
 public class JenkinsDemo {
     public static void main(String args[]){
         System.out.println("Hello!! This is my first program in Jenkins");
+		
+		System.out.println("Compiling the source code automatically...."
+		System.out.println("This is done using Git Hooks(post-commit)");
     }
 }
